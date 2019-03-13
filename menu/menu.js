e20bb37d0bf63c2504475b2cdefd162af8be2038
Ian O'Brien <iobrien@fab10.cecs.pdx.edu>
2019-03-12 18:26:24 -0700

    uploading a screenshot

--- menu/menu.js
+++ menu/menu.js
@@ -1,13 +1,10 @@
 function newMenuItem()
 {
-  var x = document.createElement("h2");
-  var y = document.createElement("h2");
-  var z = document.createElement("h2");
-
 // Create a new list item when clicking on the "Add" button function newElement() {
   var x = document.createElement("h2");
   var y = document.createElement("h2");
   var z = document.createElement("h2");
+  var split = document.createElement("hr");
   
 
 // get data from the form 
@@ -32,21 +29,26 @@ function newMenuItem()
     document.getElementById("specials").appendChild(x);
     document.getElementById("specials").appendChild(y);
     document.getElementById("specials").appendChild(z);
+    document.getElementById("specials").appendChild(z);
+    document.getElementById("specials").appendChild(split);
   }
   if (type == "entrees"){ 
     document.getElementById("entrees").appendChild(x);
     document.getElementById("entrees").appendChild(y);
     document.getElementById("entrees").appendChild(z);
+    document.getElementById("enetrees").appendChild(split);
   }
   if (type == "sides"){ 
     document.getElementById("sides").appendChild(x);
     document.getElementById("sides").appendChild(y);
     document.getElementById("sides").appendChild(z); 
+    document.getElementById("sides").appendChild(split);
   }
   if (type == "beverages") { 
     document.getElementById("beverages").appendChild(x);
     document.getElementById("beverages").appendChild(y);
     document.getElementById("beverages").appendChild(z);
+    document.getElementById("beverages").appendChild(split);
   }
   document.getElementById("type").value = "";
   document.getElementById("item").value = "";
