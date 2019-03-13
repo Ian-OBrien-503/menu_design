6568f7440a7061480349a551efea9f46a0b70fd6
Ian O'Brien <iobrien@fab10.cecs.pdx.edu>
2019-03-12 17:56:38 -0700

    finally got adding items to work

--- menu/menu.js
+++ menu/menu.js
@@ -1,38 +1,55 @@
-// Create a new list item when clicking on the "Add" button function newElement() {
-  var h2 = document.createElement("li");
+function newMenuItem()
+{
+  var x = document.createElement("h2");
+  var y = document.createElement("h2");
+  var z = document.createElement("h2");
 
-// ADDED TO BUILD MENU 
+// Create a new list item when clicking on the "Add" button function newElement() {
+  var x = document.createElement("h2");
+  var y = document.createElement("h2");
+  var z = document.createElement("h2");
+  
+
+// get data from the form 
   var type = document.getElementById("type").value;
   var item = document.getElementById("item").value;
   var description = document.getElementById("description").value;
   var price = document.getElementById("price").value;
-// ADDED TO BUILD MENU
+// create new nodes for html doc from form data 
   var addType = document.createTextNode(type);
   var addItem = document.createTextNode(item);
   var addDescription = document.createTextNode(description);
   var addPrice = document.createTextNode(price);
 
-
-  h2.appendChild(addItem);
-  document.getElementById("menu").appendchild(h2);
+// add data to the menu
+  x.appendChild(addItem);
+  y.appendChild(addDescription);
+  z.appendChild(addPrice);
   if (type === '') {
     alert("You must specify section to add to menu!");
-  } 
-  if (type == "specials"){h2.appendChild(addItem)}
-  if (type == "entrees"){}
-  if (type == "sides"){}
-  if (type == "beverages") {}
-  else {alert("specify valid type/section to add to menu")}
+  }
+  if (type == "specials"){ 
+    document.getElementById("specials").appendChild(x);
+    document.getElementById("specials").appendChild(y);
+    document.getElementById("specials").appendChild(z);
+  }
+  if (type == "entrees"){ 
+    document.getElementById("entrees").appendChild(x);
+    document.getElementById("entrees").appendChild(y);
+    document.getElementById("entrees").appendChild(z);
+  }
+  if (type == "sides"){ 
+    document.getElementById("sides").appendChild(x);
+    document.getElementById("sides").appendChild(y);
+    document.getElementById("sides").appendChild(z); 
+  }
+  if (type == "beverages") { 
+    document.getElementById("beverages").appendChild(x);
+    document.getElementById("beverages").appendChild(y);
+    document.getElementById("beverages").appendChild(z);
+  }
   document.getElementById("type").value = "";
   document.getElementById("item").value = "";
   document.getElementById("description").value = "";
   document.getElementById("price").value = "";
-
-
-
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+}
